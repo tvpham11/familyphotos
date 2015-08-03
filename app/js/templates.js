30322ea53b0291cc74797a0c0c7f0564bdbc83ac
Vi Pham <tvpham11@gmail.com>
2015-08-02 21:02:41 -0400

    updated styling

--- app/js/templates.js
+++ app/js/templates.js
@@ -2,43 +2,37 @@ this["hbs"] = this["hbs"] || {};
 this["hbs"]["main"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
     var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
-  return "    <li data-id=\""
+  return "        <li data-id=\""
     + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
-    + "\">\n      <figure>\n        <a class=\"responsive-img\" href=\"#photo/"
+    + "\">\n          <figure>\n            <a class=\"responsive-img\" href=\"#photo/"
     + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
     + "\"><img src=\""
     + alias3(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\" alt=\"france\"></a>\n        <figcaption>"
+    + "\" alt=\"france\"></a>\n            <figcaption>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
     + ": "
     + alias3(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"description","hash":{},"data":data}) : helper)))
-    + "</figcaption>\n      </figure>\n    </li>\n";
+    + "</figcaption>\n          </figure>\n        </li>\n";
 },"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
     var stack1;
 
-  return "<h2>France December 2014</h2>\n\n<form id=\"addPhoto\">\n  <input type=\"text\" placeholder=\"Photo URL\" id=\"photoURL\">\n  <input type=\"text\" placeholder=\"Photo Title\" id=\"photoTitle\">\n  <input type=\"text\" placeholder=\"Photo Description\" id=\"photoDesc\">\n  <button class=\"btn waves-effect waves-light\" type=\"submit\" name=\"action\">Submit\n    <i class=\"mdi-content-send right\"></i>\n  </button>\n</form>\n\n<ul>\n"
+  return "<h2>France: December 2014</h2>\n\n<div class=\"row\">\n  <form id=\"addPhoto\" class=\"col l5\">\n    <input type=\"text\" placeholder=\"Photo URL\" id=\"photoURL\">\n    <input type=\"text\" placeholder=\"Photo Title\" id=\"photoTitle\">\n    <input type=\"text\" placeholder=\"Photo Description\" id=\"photoDesc\">\n    <button class=\"btn waves-effect waves-light\" type=\"submit\" name=\"action\">Submit\n      <i class=\"mdi-content-send right\"></i>\n    </button>\n  </form>\n\n  <div class=\"col l7\">\n    <ul class=\"row\">\n"
     + ((stack1 = helpers.each.call(depth0,(depth0 != null ? depth0.photo : depth0),{"name":"each","hash":{},"fn":this.program(1, data, 0),"inverse":this.noop,"data":data})) != null ? stack1 : "")
-    + "</ul>\n\n";
+    + "    </ul>\n  </div>\n</div>\n";
 },"useData":true});
 this["hbs"] = this["hbs"] || {};
-this["hbs"]["single"] = Handlebars.template({"1":function(depth0,helpers,partials,data) {
-    return "    <p>This has been visited</p>\n";
-},"3":function(depth0,helpers,partials,data) {
-    return "    <p>This has NOT been visited</p>\n";
-},"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
-    var stack1, helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
+this["hbs"]["single"] = Handlebars.template({"compiler":[6,">= 2.0.0-beta.1"],"main":function(depth0,helpers,partials,data) {
+    var helper, alias1=helpers.helperMissing, alias2="function", alias3=this.escapeExpression;
 
   return "<div class=\"photo panel\">\n\n  <h3>"
     + alias3(((helper = (helper = helpers.title || (depth0 != null ? depth0.title : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"title","hash":{},"data":data}) : helper)))
-    + "</h3>\n  <p><a href=\""
+    + "</h3>\n  <a href=\""
     + alias3(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
-    + "\">"
+    + "\"><img src=\""
     + alias3(((helper = (helper = helpers.url || (depth0 != null ? depth0.url : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"url","hash":{},"data":data}) : helper)))
-    + "</a></p>\n  <p>"
+    + "\" alt=\"france\"></a>\n  <p>"
     + alias3(((helper = (helper = helpers.description || (depth0 != null ? depth0.description : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"description","hash":{},"data":data}) : helper)))
-    + "</p>\n\n"
-    + ((stack1 = helpers['if'].call(depth0,(depth0 != null ? depth0.visited : depth0),{"name":"if","hash":{},"fn":this.program(1, data, 0),"inverse":this.program(3, data, 0),"data":data})) != null ? stack1 : "")
-    + "\n  <button id=\"delete\" class=\"alert\" data-id=\""
+    + "</p>\n\n\n  <button id=\"delete\" class=\"alert\" data-id=\""
     + alias3(((helper = (helper = helpers._id || (depth0 != null ? depth0._id : depth0)) != null ? helper : alias1),(typeof helper === alias2 ? helper.call(depth0,{"name":"_id","hash":{},"data":data}) : helper)))
     + "\">Delete</button>\n\n</div>\n";
 },"useData":true});
--- app/js/views/main.view.js
+++ app/js/views/main.view.js
@@ -1,4 +1,4 @@
-;(function () {
+ ;(function () {
 
   'use strict';
   app.Views.Main = Backbone.View.extend({
